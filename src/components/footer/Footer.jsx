c9b3dc9f922712c08d0b1df0337f22bd5356853f
unknown <bhabishyaluitel@gmail.com>
2022-06-09 15:35:28 +0545

    First Commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,33 +1,33 @@
-// /* eslint-disable jsx-a11y/anchor-is-valid */
-// import React from 'react'
-// import './footer.css'
-// import {AiOutlineTwitter} from 'react-icons/ai'
-// import {SiUpwork} from 'react-icons/si'
-// import {SiGmail} from 'react-icons/si'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import './footer.css'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {SiUpwork} from 'react-icons/si'
+import {SiGmail} from 'react-icons/si'
 
-// const Footer = () => {
-//   return (
-//     <Footer>
-//       <a href="#" className='footer__logo'>Jitan</a>
+const Footer = () => {
+  return (
+    <Footer>
+      <a href="#" className='footer__logo'>Jitan</a>
 
-//       <ul className='permalinks'>
-//         <li><a href="#">Home</a></li>
-//         <li><a href="#">About</a></li>
-//         <li><a href="#">Experience</a></li>
-//         <li><a href="#">Services</a></li>
-//         <li><a href="#">Portfolio</a></li>
-//         <li><a href="#">Testimonials</a></li>
-//         <li><a href="#">Contact</a></li>
-//       </ul>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Experience</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Testimonials</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
 
-//       <div className='footer__socials'>
-//         <a href="https://twitter.com"><AiOutlineTwitter/></a>
-//         <a href="https://upwork.com"><SiUpwork/></a>
-//         <a href="https://gmail.com"><SiGmail/></a>
+      <div className='footer__socials'>
+        <a href="https://twitter.com"><AiOutlineTwitter/></a>
+        <a href="https://upwork.com"><SiUpwork/></a>
+        <a href="https://gmail.com"><SiGmail/></a>
 
-//       </div>
-//     </Footer>
-//   )
-// }
+      </div>
+    </Footer>
+  )
+}
 
-// export default Footer
+export default Footer
